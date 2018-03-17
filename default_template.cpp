d9d79d62e1f9ab1de039022b216ae568540554df
siriusdark <subhampal198@gmail.com>
2018-03-17 13:17:36 +0530

    Added default template

---
+++ default_template.cpp
@@ -0,0 +1,29 @@
+//Dream your Code, Code your Dream [Siriuslight -Subham Pal]
+#include<bits/stdc++.h>
+#define FIO ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+using namespace std;    typedef long long ll;    typedef long double ld;
+
+const int inf = 1e9;
+const ll INF = 1e18, MOD = 1e9+7;;
+
+ll pow(ll a, ll b){
+	ll res = 1;
+	while(b){
+		if(b & 1) res = (res * a) % MOD;
+		a = (a * a) % MOD;
+		b = b >> 1;
+	}
+	return res;
+}
+
+ll gcd(ll a, ll b){
+	if(a < b) return gcd(b,a);
+	if(!b)	return a;
+	return gcd(b, a % b);
+}
+
+int main(){
+    FIO;
+	
+    return 0;
+}
