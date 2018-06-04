87e585be248c3ba99b72dcf2e62d605993f3acc9
Subham <subhampal198@gmail.com>
2018-06-05 00:35:16 +0530

    Update default_template.cpp

--- default_template.cpp
+++ default_template.cpp
@@ -5,7 +5,7 @@ using namespace std;    typedef long long ll;    typedef long double ld;
 const int inf = 1e9, N = 1e6+5;
 const ll INF = 1e18, MOD = 1e9+7;;
 ll power(ll a,ll b,ll m=MOD){ll r=1;while(b>0){if(b&1)r=r*a%m;a=a*a%m;b>>=1;}return r;}
-ll gcd(ll a, ll b){if(b>a)swap(a,b);if(!b)return a;return gcd(b, a % b);}
+ll gcd(ll a,ll b){if(b>a)swap(a,b);if(!b)return a;return gcd(b,a%b);}
 
 int main(){
     FIO;
