9ccabc424ff9f99a388d4f9d128f81956dc05d85
Subham <subhampal198@gmail.com>
2018-10-23 14:38:37 +0530

    Update default_template.cpp

--- default_template.cpp
+++ default_template.cpp
@@ -1,15 +1,23 @@
-//Dream your Code, Code your Dream [Siriuslight -Subham Pal]
 #include<bits/stdc++.h>
-#define FIO ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
-using namespace std;    typedef long long ll;    typedef long double ld;
-const int inf = 1e9, N = 1e6+5;
-const ll INF = 1e18, MOD = 1e9+7;;
-ll power(ll a,ll b,ll m=MOD){ll r=1;while(b>0){if(b&1)r=r*a%m;a=a*a%m;b>>=1;}return r;}
-ll gcd(ll a,ll b){if(b>a)swap(a,b);if(!b)return a;return gcd(b,a%b);}
+#define FIO ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0), cout << setprecision(10) << fixed;
+#define F first
+#define S second
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+const ll inf = 1e9+100, N = 2e5+5;
+const ll INF = 1e18+100, MOD = 1e9+7;
+
+ll powmod(ll a,ll b,ll m=MOD){ll r=1;while(b>0){if(b&1)r=r*a%m;a=a*a%m;b>>=1;}return r;}
+ll power(ll a,ll b){ll r=1;while(b>0){if(b&1)r=r*a;a=a*a;b>>=1;}return r;}
+ll gcd(ll a,ll b){if(!b)return a;return gcd(b,a%b);}
+ll inv(ll a, ll m = MOD){return powmod(a,m-2,m);}
+ll lcm(ll a,ll b){return(a*b)/gcd(a,b);}
 
 int main(){
-    FIO;
-    cout << setprecision(10) << fixed;
+	FIO;
+	
 
-    return 0;
 }
