ea7e98b18349a1b0144641e54bf8c7bdc85e79c0
Subham <subhampal198@gmail.com>
2019-04-19 01:41:26 +0530

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,15 +1,25 @@
-void update(int id, int val){
-    while(id <= n){
-        BIT[id] += val;
-        id += (id & -id);
-    }
-}
+template <class Type>
+class FenwickTree{
+	Type T[N];
+	FenwickTree(){
+		fill(T, T + N, 0);
+	}
+	
+	void update(int index, int value){
+		while(id <= n){
+			T[id] += value;
+			id += (id & -id);
+		}
+	}
 
-int query(int id){
-    int res = 0;
-    while(id >= 1){
-        res += BIT[id];
-        id -= (id & -id);
-    }
-    return res;
-}
+	Type query(int id){
+		int res = 0;
+		while(id >= 1){
+			res += T[id];
+			id -= (id & -id);
+		}
+		return res; 
+	}
+};
+
+FenwickTree FT;
